b0e2ddf5acbfca766be7b0ee0e287d573c1bf959
ni4in <to.nithinv@gmail.com>
2024-03-10 20:32:14 +0530

    secod commit

---
+++ king-book/chapter-4/ex5.c
@@ -0,0 +1,10 @@
+/*Rewrite the upc.c program so that user enters 11 digits at one time*/
+
+#include<stdio.h>
+int main()
+{
+    int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11;
+    printf("Enter the 11 digit number : ");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d");
+    
+}
