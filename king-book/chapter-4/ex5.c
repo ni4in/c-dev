43f564556da0dcbda1a33866364da4e9cd5d716e
nithin <to.nithinv@gmail.com>
2024-03-11 10:25:49 +0530

    added the exe5

--- king-book/chapter-4/ex5.c
+++ king-book/chapter-4/ex5.c
@@ -3,8 +3,13 @@
 #include<stdio.h>
 int main()
 {
-    int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11;
+    int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11,odd_sum, even_sum,total, check_digit;
     printf("Enter the 11 digit number : ");
-    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d");
-    
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",&d1,&d2,&d3,&d4,&d5,&d6,&d7,&d8,&d9,&d10,&d11);
+    odd_sum = d1 + d3 + d5 + d7 + d9 + d11;
+    even_sum = d2 + d4 + d6 + d8 + d10;
+    total = 3*odd_sum + even_sum;
+    check_digit = 9-((total-1)%10);
+    printf("Check Digit: %d \n", check_digit);
+    return 0;
 }
